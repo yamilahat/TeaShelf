895c3c5567b42eb51f2064d7b26b30f1007651a0
yamilahat <yamilahat@gmail.com>
2026-03-21 17:40:35 +0200

    fix: vite config host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host:true,
     proxy: {
       "/api": {
         target: "http://localhost:8000",
